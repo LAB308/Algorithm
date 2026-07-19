5c517fce16f213b1b118a578c081d10cabdc9940
HongChangMo <hcm7402@naver.com>
2026-07-19 10:35:53 +0900

    update

---
+++ 프로그래머스/_20260719/타겟넘버_43165.java
@@ -0,0 +1,23 @@
+package 프로그래머스._20260719;
+
+public class 타겟넘버_43165 {
+
+    private static boolean[] VISITED;
+
+    public static void main() {
+
+        int[] numbers = {1, 1, 1, 1, 1};
+        int target = 3;
+
+        solution(numbers, target);
+    }
+
+    public static int solution(int[] numbers, int target) {
+        int answer = 0;
+        VISITED = new boolean[numbers.length];
+
+
+
+        return answer;
+    }
+}
